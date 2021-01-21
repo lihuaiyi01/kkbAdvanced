2602d3b5589e5b5566cfe3157f1dd5a2d7f00624
lihuaiyi01 <1195841709@qq.com>
2021-01-21 16:29:45 +0800

    111

--- 前后端交互/index.js
+++ 前后端交互/index.js
@@ -1,39 +1,27 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const static = require('koa-static')
-const usersData = require('./data/users.json')
-console.log(usersData);
+const koaBody = require('koa-body')
+const fs = require('fs')
 
 let app = new Koa()
 let router = new Router()
 
 app.use(static(__dirname + '/static'))
+app.use(koaBody({
+    multipart: true
+}))
 
 router.get('/', (ctx, next) => {
     ctx.body = 'hello'
 })
-router.get('/checkUserName', (ctx, next) => {
-    console.log(ctx.query.username);
-    let res = usersData.find(v => v.username == ctx.query.username)
-    if (res) {
-        ctx.body = {
-            status: 1,
-            info: '用户名正确'
-        }
-    } else {
-        ctx.body = {
-            status: 2,
-            info: '用户名错误'
-        }
-    }
-})
-router.get('/get/:id', (ctx, next) => {
-    console.log(ctx.params);
-    ctx.body = {
-        status: 1,
-        info: '请求成功'
-    }
+router.post("/upload", (ctx, next) => {
+    // console.log(ctx.request.body);
+    // console.log(ctx.request.files.img);
+    let fileData = fs.readFileSync(ctx.request.files.img.path);
+    fs.writeFileSync("static/imgs/" + ctx.request.files.img.name, fileData);
+    ctx.body = "请求成功";
 })
 
 app.use(router.routes())
-app.listen(3001)
+app.listen(3000)
