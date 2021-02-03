b1f3393c8a51e9bd2cae918913e1ba19b016fd03
lihuaiyi01 <1195841709@qq.com>
2021-02-03 17:11:30 +0800

    111

--- http4000.js
+++ http4000.js
@@ -1,28 +1,20 @@
-const Koa = require("koa");
-const static = require("koa-static");
-const Router = require("koa-router");
-let app = new Koa();
-let router = new Router();
-app.use(static(__dirname + "/static"));
+const Koa = require('koa')
+const Router = require('koa-router')
+const static = require('koa-static')
 
-router.get("/", (ctx, next) => {
-    ctx.body = "hello run at 4000";
+let app = new Koa()
+let router = new Router()
+
+app.use(static(__dirname + '/static'))
+router.get('/', (ctx, next) => {
+    ctx.body = 'hello'
 })
-router.get("/getAjax", (ctx, next) => {
-    console.log("4000 run ");
-    // console.log(ctx.query.name);
-    // ctx.body = {
-    //     name:"李四4000",
-    //     age:20
-    // }
-    let cb = ctx.query.cb;
-    // ctx.body = "var a = 20";
-    let obj = {
-        a: 20,
-        b: 20
+router.get('/getAjax', (ctx, next) => {
+    ctx.body = {
+        name: '李四4000',
+        age: 20
     }
-    ctx.body = `${cb}(${JSON.stringify(obj)})`;
 })
 
-app.use(router.routes());
-app.listen(4000);
+app.use(router.routes())
+app.listen(4000)
