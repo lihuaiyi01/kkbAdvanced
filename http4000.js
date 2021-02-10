2eadc81bcfd492b6594de29479a03fd12fb9c3ec
lihuaiyi01 <1195841709@qq.com>
2021-02-10 23:02:33 +0800

    111

--- http4000.js
+++ http4000.js
@@ -7,13 +7,22 @@ let router = new Router()
 
 app.use(static(__dirname + '/static'))
 router.get('/', (ctx, next) => {
-    ctx.body = 'hello'
+    ctx.body = 'hello run at 4000'
 })
 router.get('/getAjax', (ctx, next) => {
-    ctx.body = {
-        name: '李四4000',
-        age: 20
+    console.log("4000 run");
+    // console.log(ctx.query.name);
+    // ctx.body = {
+    //     name: '李四4000',
+    //     age: 20
+    // }
+    let cb = ctx.query.callback
+    // ctx.body = "var a = 20"
+    let obj = {
+        a: 20,
+        b: 20
     }
+    ctx.body = `${cb}(${JSON.stringify(obj)})`
 })
 
 app.use(router.routes())
